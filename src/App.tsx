d6a0061666eb1fef7e0afc2a16afa7de9bd20b69
keyka <tkwsm1221@gmail.com>
2025-05-22 23:37:21 +0900

    Headerにハンバーガーメニューを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
+  const [SideMenuOpen, setSideMenuOpen] = useState(false);
+    //SideMenuOpenっていうStateをfalseからスタート。setSideMenuOpenはセッター。
+
+  const toggleSideMenu = () => {
+    setSideMenuOpen(prev => !prev);
+      //prev=前の状態。falseならtrueへ
+  };
   return (
     <div className="header">
-      <Header title="ユーザー管理するよ！"/>
+      <Header title="ユーザー管理するよ！" onMenuClick = {toggleSideMenu}/>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function Header(props: { title: string }) {
-  return <h1>{props.title}</h1>;
+function Header(props: { title: string ,onMenuClick : () => void}) {
+    //Headerを使うときに、Stringを渡してね
+    //あと、onMenuClickって名前の引数なし、戻り値なしの関数の中身も書いてね
+  return (
+    <>
+      <i className="fas fa-bars" onClick={props.onMenuClick}></i>
+      <h1>{props.title}</h1>
+    </>
+  );
 }
 
 export default Header;
